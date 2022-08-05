6dc311a27b1b8ea1d2123039d7b650b70a30fcee
bdiddy2022 <brendan.t.downs@gmail.com>
2022-08-04 20:36:29 -0400

    INITIAL COMMIT - ALL TESTS PASSED

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -2,34 +2,42 @@ package main;
 
 
 public class BalancedBrackets {
-    /**
-     * The function BalancedBrackets should return true if and only if
-     * the input string has a set of "balanced" brackets.
-     *
-     * That is, whether it consists entirely of pairs of opening/closing
-     * brackets (in that order), none of which mis-nest. We consider a bracket
-     * to be square-brackets: [ or ].
-     *
-     * The string may contain non-bracket characters as well.
-     *
-     * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
-     * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
-     *
-     * @param str - to be validated
-     * @return true if balanced, false otherwise
-     */
+//    /**
+//     * The function BalancedBrackets should return true if and only if
+//     * the input string has a set of "balanced" brackets.
+//     *
+//     * That is, whether it consists entirely of pairs of opening/closing
+//     * brackets (in that order), none of which mis-nest. We consider a bracket
+//     * to be square-brackets: [ or ].
+//     *
+//     * The string may contain non-bracket characters as well.
+//     *
+//     * These strings have balanced brackets:
+//     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+//     *
+//     * While these do not:
+//     *   "[LaunchCode", "Launch]Code[", "[", "]["
+//     *
+//     * @param str - to be validated
+//     * @return true if balanced, false otherwise
+//     */
     public static boolean hasBalancedBrackets(String str) {
+        char lastBracket = 'c';
         int brackets = 0;
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
                 brackets++;
+                if (lastBracket == ']') {
+                    return false;}
+                lastBracket = ch;
             } else if (ch == ']') {
                 brackets--;
+                lastBracket = ch;
+
             }
         }
         return brackets == 0;
     }
+
+
 }
